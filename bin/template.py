60cc65b4074715e0ebed2f77340af9360c8c598a
Dann Bohn <dbohn@opengov.com>
2025-05-27 16:23:21 -0400

    template

--- bin/template.py
+++ bin/template.py
@@ -6,17 +6,32 @@ import shutil
 
 def set_variables():
   suite = os.environ.get('SUITE')
+  if suite is None:
+    raise ValueError('SUITE environment variable is required')
   name = os.environ.get('NAME')
+  if name is None:
+    raise ValueError('NAME environment variable is required')
   okta_group = os.environ.get('OKTA_GROUP')
+  if okta_group is None:
+    raise ValueError('OKTA_GROUP environment variable is required')
   ecr_repo = os.environ.get('ECR_REPO')
+  if ecr_repo is None:
+    raise ValueError('ECR_REPO environment variable is required')
   gitops_repo = os.environ.get('GITOPS_REPO')
+  if gitops_repo is None:
+    raise ValueError('GITOPS_REPO environment variable is required')
+  kargo_project_name= os.environ.get('KARGO_PROJECT_NAME')
+  if kargo_project_name is None:
+    kargo_project_name = f"{suite}-{name}-project"
 
   return {
     'suite': suite,
     'name': name,
     'okta_group': okta_group,
     'ecr_repo': ecr_repo,
-    'gitops_repo': gitops_repo,}
+    'gitops_repo': gitops_repo,
+    'kargo_project_name': kargo_project_name,
+  }
 
 
 os.makedirs('rendered', exist_ok=True)
